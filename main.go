33465cacdb06472f11f628f3d6cc1693dfc84c1f
Veerasolaiyappan <vveeraso@redhat.com>
2025-06-25 11:53:37 +0530

    fix: listen on 0.0.0.0 for OpenShift

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
-// main.go
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
-
-	r.Run(":8080") // listens on port 8080
+	r.Run("0.0.0.0:8080") // 👈 Bind to all interfaces
 }
