36883661c32c7521d8b91c32af3af0ad11c7571f
Veerasolaiyappan <vveeraso@redhat.com>
2025-07-06 13:12:04 +0530

    Updated the  response

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello world",
+			"message": "Hello world !!",
 		})
 	})
 	r.Run("0.0.0.0:8080") // 👈 Bind to all interfaces
